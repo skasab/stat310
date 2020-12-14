5ef668b291e4147b94848374bc48fc29a792a960
Solly Kasab <sollykasab@gmail.com>
2020-12-13 22:29:34 -0500

    added margins between elections

--- shiny/server.R
+++ shiny/server.R
@@ -6,7 +6,7 @@ library(rjson)
 shinyServer(function(input, output, session) {
 
     counties <- rjson::fromJSON(file='https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
-    data <- read_csv("../data/countypres_2000-2016.csv")
+    data <- read_csv("data/countypres_2000-2016.csv")
     
     
     #remove green party candidate and add to Other total for 2000 election
@@ -16,20 +16,39 @@ shinyServer(function(input, output, session) {
         mutate(candidatevotes = sum(candidatevotes)) %>% 
         filter(candidate == "Other")
     
+    # combine with the rest of the data
     data <- data %>%
         filter((year != 2000 | candidate != "Other")) %>%
-        bind_rows(others2000)
+        bind_rows(others2000) %>%
+        filter(candidate != "Ralph Nader")
     
     #remove empty data points and generally clean data for usage
     data <- data %>% 
         drop_na(FIPS) %>%
         replace_na(list(party = "other",candidatevotes = 0)) %>%
         filter(totalvotes != 0) %>%
+        filter(state != "Alaska") %>%
         mutate(fips5 = sprintf("%05d",FIPS)) %>%
         select(-version,office) %>% 
-        mutate(percent = candidatevotes/totalvotes)
+        mutate(percent = candidatevotes/totalvotes) %>%
+        arrange(year,state,FIPS)
     
-    dataFiltered <- reactive({
+    getPrior <- function(theYear,theParty,theFips) {
+        returnValue <- filter(data,year == theYear - 4, party == theParty, FIPS == theFips)
+        if(nrow(returnValue) == 0) {
+            tmp <- filter(data,year == theYear, party == theParty, FIPS == theFips)
+            return(tmp$percent)
+        }
+        return(returnValue$percent)
+    }
+    
+    margins <- data %>%
+        filter(year != 2000) %>%
+        mutate(tmp = pmap(list(year,party,FIPS),getPrior)) %>%
+        mutate(priorPercent = flatten_dbl(tmp)) %>%
+        select(-tmp)
+    
+    dataFiltered <- eventReactive(input$go, {
         if(input$party == "both") {
             parties = c("democrat","republican")   
         }
@@ -38,15 +57,24 @@ shinyServer(function(input, output, session) {
         }
         
         dataFiltered <- data %>% filter(year == input$year, party %in% parties)
-        # print(input$year)
-        # print(parties)
-        #print(dataFiltered)
         return(dataFiltered)
+    })
+    
+    marginsFiltered <- eventReactive(input$go, {
+        if(input$party == "both") {
+            parties = c("democrat","republican")   
+        }
+        else {
+            parties = input$party
+        }
+        
+        marginsFiltered <- margins %>% filter(year == input$year, party %in% parties)
+        return(marginsFiltered)
     })
     
     candidates <- reactiveValues(democrat = NULL, republican = NULL, other = NULL)
     
-    observe({
+    observeEvent(input$go,{
         if(input$year == 2000) {
             candidates$democrat <-  "Al Gore"
             candidates$republican <- "George W. Bush"
@@ -76,22 +104,29 @@ shinyServer(function(input, output, session) {
     
     stylings <- reactiveValues(colorScale = NULL, colorScaleTitle = NULL, plotTitle = NULL, reverse = NULL)
     
-    observe({
+    observeEvent(input$go,{
+        if(input$shift == TRUE) {
+            base <- "Vote Shift To"
+        }
+        else {
+            base <- "Vote Percent For"
+        }
+        
         if(input$party == "democrat") {
             stylings$colorScale <- "Blues"
-            stylings$colorScaleTitle <- paste("Vote Percent For",candidates$democrat)
+            stylings$colorScaleTitle <- paste(base,candidates$democrat)
             stylings$plotTitle <- paste(input$year,"Presidential Election, Votes for",candidates$democrat)
             stylings$reverse <- TRUE
         }
         else if(input$party == "republican") {
             stylings$colorScale <- "Reds"
-            stylings$colorScaleTitle <- paste("Vote Percent For",candidates$republican)
+            stylings$colorScaleTitle <- paste(base,candidates$republican)
             stylings$plotTitle <- paste(input$year,"Presidential Election, Votes for",candidates$republican)
             stylings$reverse <- FALSE
         }
         else if(input$party == "other") {
             stylings$colorScale <- "Greys"
-            stylings$colorScaleTitle <- paste("Vote Percent For",candidates$other)
+            stylings$colorScaleTitle <- paste(base,candidates$other)
             stylings$plotTitle <- paste(input$year,"Presidential Election, Votes for",candidates$other)
             stylings$reverse <- TRUE
         }
@@ -100,6 +135,23 @@ shinyServer(function(input, output, session) {
             stylings$colorScaleTitle <- "Two-Party Vote<br>(Scale: 1 = More Republican, 0 = More Democratic)"
             stylings$plotTitle <- paste(input$year,"Presidential Election")
             stylings$reverse <-  FALSE
+        }
+        
+        if(input$shift == TRUE) {
+            stylings$plotTitle = paste("Shift in Presidential Vote Margin,",(input$year - 4),"to",input$year) 
+            if(input$party == "democrat") {
+                stylings$colorScale <- "RdBu"
+                stylings$reverse <-  TRUE
+            }
+            if(input$party == "republican") {
+                stylings$colorScale <- "RdBu"
+                stylings$reverse <-  FALSE
+            }
+            if(input$party == "both") {
+                stylings$colorScale <- "Picnic"
+                stylings$colorScaleTitle <- "Two-Party Vote<br>(Scale: (+) = More Republican, (-) = More Democratic)"
+                stylings$reverse <-  FALSE
+            }
         }
 
         # print(stylings$colorScale)
@@ -110,14 +162,14 @@ shinyServer(function(input, output, session) {
     
     output$table <- renderTable(dataFiltered())
 
-    output$distPlot <- renderPlotly({
+    getValuePlot <- function() {
         plotdata <- dataFiltered()
         
         plotdata <- plotdata %>% 
             group_by(input$year, state, FIPS, fips5) %>% 
             mutate(tmp1 = sum(candidatevotes),tmp2 = tmp1 - candidatevotes, oppPercent = tmp2 / totalvotes) %>%
             select(-tmp1,tmp2)
-
+        
         usaConfig <- list(
             scope = 'usa',
             projection = list(type = 'albers usa'),
@@ -151,6 +203,7 @@ shinyServer(function(input, output, session) {
                             TRUE ~ paste0(base,candidates$republican,"<br>",round(plotdata$oppPercent * 100),"% ",candidates$democrat)))
                 },
                 zmin=0,
+                zmid=.5,
                 zmax=1,
                 colorscale = stylings$colorScale,
                 reversescale = stylings$reverse,
@@ -161,8 +214,87 @@ shinyServer(function(input, output, session) {
             colorbar(title = stylings$colorScaleTitle) %>%
             layout(title = stylings$plotTitle) %>%
             layout(geo = usaConfig)
-           
-        fig
+        
+        return(fig)
+    }
+    
+    getMarginPlot <- function() {
+        plotdata <- marginsFiltered()
+    
+        plotdata <- plotdata %>% 
+            mutate(marginShift = percent - priorPercent)
+                
+        usaConfig <- list(
+            scope = 'usa',
+            projection = list(type = 'albers usa'),
+            showlakes = TRUE,
+            lakecolor = toRGB('white')
+        )
+        fig <- plot_ly() %>% 
+            add_trace(
+                type="choropleth",
+                geojson=counties,
+                locations=plotdata$fips5,
+                z=plotdata$marginShift,
+                hoverinfo = 'text',
+                # text = paste0(plotdata$county, " County<br>",
+                #               round(plotdata$percent * 100),"% ",
+                #               case_when(
+                #                   input$party == "democrat" ~ candidates$democrat,
+                #                   input$party == "republican" ~ candidates$republican,
+                #                   input$party == "other" ~ candidates$other,
+                #                   input$party == "both" ~ paste0(candidates$republican,"<br>",round(plotdata$oppPercent * 100),"%",candidates$democrat))),
+                # 
+                text = {
+                    base <- paste0(plotdata$county, " County<br>",abs(round(plotdata$marginShift * 100)),"% ")
+                    
+                    case_when(
+                        input$party == "democrat" ~ case_when(
+                            plotdata$marginShift >= 0 ~ paste0(base,"towards ",candidates$democrat, " since ", (input$year - 4)),
+                            plotdata$marginShift < 0 ~ paste0(base,"away from ",candidates$democrat, " since ", (input$year - 4))),
+                        input$party == "republican" ~ case_when(
+                            plotdata$marginShift >= 0 ~ paste0(base,"towards ",candidates$republican, " since ", (input$year - 4)),
+                            plotdata$marginShift < 0 ~ paste0(base,"away from ",candidates$republican, " since ", (input$year - 4))),
+                        input$party == "other" ~ case_when(
+                            plotdata$marginShift >= 0 ~ paste0(base,"towards ",candidates$other, " since ", (input$year - 4)),
+                            plotdata$marginShift < 0 ~ paste0(base,"away from ",candidates$other, " since ", (input$year - 4))),
+                        input$party == "both" ~ case_when(
+                            plotdata$marginShift >= 0 ~ paste0(base,"towards the Republican (",candidates$republican,") since ", (input$year - 4)),
+                            plotdata$marginShift < 0 ~ paste0(base,"towards the Democrat (",candidates$democrat,") since ", (input$year - 4))))
+                },
+                zmin=--1,
+                zmid=0,
+                zmax=1,
+                colorscale = stylings$colorScale,
+                reversescale = stylings$reverse,
+                marker=list(line=list(
+                    width=.1,
+                    color="#111111")
+                )) %>%
+            colorbar(title = stylings$colorScaleTitle) %>%
+            layout(title = stylings$plotTitle) %>%
+            layout(geo = usaConfig)
+        
+        return(fig)
+    }
+    
+    thePlot <- eventReactive(input$go, {
+        if(input$shift) {
+            if(input$year == 2000) {
+                
+            }
+            else {
+                return(getMarginPlot())
+            }
+        }
+        else {
+            return(getValuePlot())
+        }  
     })
+    
+    output$plot <- renderPlotly({
+        thePlot()
+    })    
+
 
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,10 +1,12 @@
 library(shiny)
 library(plotly)
+library(shinycssloaders)
 
 shinyUI(fluidPage(
 
     titlePanel("Solly Kasab Final Project"),
     helpText("Note: Plotly's maps are quite slow, so please be patient whilst the map loads."),
+    helpText("(in some cases, very patient)"),
     hr(),
     tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"), #removes minor ticks on year selector
     
@@ -18,13 +20,16 @@ shinyUI(fluidPage(
                         value = 2000,
                         sep = ''),
             radioButtons("party", label = h3("Party"),
-                         choices = list("Two-Party" = "both","Democrat" = "democrat","Republican" = "republican", "Other" = "other"))
-            # checkboxInput("shift","Show margin shift from previous election (2004 onward)",value = FALSE)
+                         choices = list("Two-Party" = "both","Democrat" = "democrat","Republican" = "republican", "Other" = "other")),
+            checkboxInput("shift","Show margin shift from previous election (2004 onward)",value = FALSE),
+            actionButton("go", label = "Visualize election!"),
         ),
 
         mainPanel(
-            plotlyOutput("distPlot")
-            #,tableOutput("table")
+            # conditionalPanel("input.shift == FALSE || input.year != 2000" , plotlyOutput("distPlot")),
+            # conditionalPanel("input.shift == TRUE && input.year == 2000" , p("Cannot select margin map for year 2000"))
+            # #,tableOutput("table")
+            plotlyOutput("plot") %>% withSpinner(type = 3, size = 2, color.background = "#FFFFFF")
         )
     )
 ))
